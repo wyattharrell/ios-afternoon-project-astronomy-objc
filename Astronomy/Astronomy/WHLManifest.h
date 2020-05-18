dcd205a9a1d68813a7e598be91d7d68e6c2a5d3e
Hector <hectoremil21@hotmail.com>
2020-05-18 17:36:32 -0400

    Properties in Manifest.h

--- Astronomy/Astronomy/WHLManifest.h
+++ Astronomy/Astronomy/WHLManifest.h
@@ -12,6 +12,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface WHLManifest : NSObject
 
+@property (nonatomic, readonly) int soldID;
+@property (nonatomic, readonly) int photoCount;
+@property (nonatomic, readonly, copy) NSArray<NSString *> *cameras;
+
 @end
 
 NS_ASSUME_NONNULL_END
