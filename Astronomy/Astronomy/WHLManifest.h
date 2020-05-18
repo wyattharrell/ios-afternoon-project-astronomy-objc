7f46e3249657f27bdee7f0a6952908237579942a
Hector <hectoremil21@hotmail.com>
2020-05-18 17:34:24 -0400

    Create WHLManifest files

---
+++ Astronomy/Astronomy/WHLManifest.h
@@ -0,0 +1,17 @@
+//
+//  WHLManifest.h
+//  Astronomy
+//
+//  Created by Karen Rodriguez on 5/18/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WHLManifest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Astronomy/Astronomy/WHLManifest.m
@@ -0,0 +1,13 @@
+//
+//  WHLManifest.m
+//  Astronomy
+//
+//  Created by Karen Rodriguez on 5/18/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+#import "WHLManifest.h"
+
+@implementation WHLManifest
+
+@end
