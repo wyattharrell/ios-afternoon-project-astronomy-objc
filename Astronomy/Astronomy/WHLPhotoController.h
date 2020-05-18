8d3578a27c6e07fe24a2d890565e04f7fd174d98
Hector <hectoremil21@hotmail.com>
2020-05-18 16:24:36 -0400

    Create WHL Prefixed PhotoController files in ObjC

---
+++ Astronomy/Astronomy/WHLPhotoController.h
@@ -0,0 +1,17 @@
+//
+//  WHLPhotoController.h
+//  Astronomy
+//
+//  Created by Karen Rodriguez on 5/18/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WHLPhotoController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Astronomy/Astronomy/WHLPhotoController.m
@@ -0,0 +1,13 @@
+//
+//  WHLPhotoController.m
+//  Astronomy
+//
+//  Created by Karen Rodriguez on 5/18/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+#import "WHLPhotoController.h"
+
+@implementation WHLPhotoController
+
+@end
