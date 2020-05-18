c21767b6bdbea4c10aaec5eddd3b8d42f54465f1
Hector <hectoremil21@hotmail.com>
2020-05-18 15:29:53 -0400

    PhotoDetailVC print test

--- Astronomy/Astronomy/View Controllers/PhotoDetailViewController.swift
+++ Astronomy/Astronomy/View Controllers/PhotoDetailViewController.swift
@@ -14,6 +14,7 @@ class PhotoDetailViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        print("Potato")
     }
     
 
