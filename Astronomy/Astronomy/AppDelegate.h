a2ce5fdaa8482918a0f81b6c6a1c7e23f22b68e0
Wyatt Harrell <wyattaharrell@gmail.com>
2020-05-18 15:10:59 -0400

    Created Xcode project

---
+++ Astronomy/Astronomy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Astronomy
+//
+//  Created by Wyatt Harrell on 5/18/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
