bc8c3bbe7223aab24b4a539bc338d4bf043086fb
Hector <hectoremil21@hotmail.com>
2020-05-18 16:23:17 -0400

    Implement Photo Class

--- Astronomy/Astronomy/Photo.swift
+++ Astronomy/Astronomy/Photo.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+@objc class Photo: NSObject {
+    @objc let sol: Int
+    @objc let cameraName: String
+    @objc let cameraFullName: String
+    @objc let photoDate: Date
+    @objc let imgSrc: URL
+    @objc let roverID: Int
+    @objc let roverName: String
+
+    @objc init(sol: Int, cameraName: String, cameraFullName: String, photoDate: Date, imgSrc: URL, roverID: Int, roverName: String) {
+        self.sol = sol
+        self.cameraName = cameraName
+        self.cameraFullName = cameraFullName
+        self.photoDate = photoDate
+        self.imgSrc = imgSrc
+        self.roverID = roverID
+        self.roverName = roverName
+    }
+}
