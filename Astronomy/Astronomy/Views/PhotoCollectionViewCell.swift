8fb604632bc67bf22602afe2beaa7525f688ebda
Wyatt Harrell <wyattaharrell@gmail.com>
2020-05-18 15:16:53 -0400

    Created CollectionView, CollectionViewCell, DetailViewController, reorganized files

---
+++ Astronomy/Astronomy/Views/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Astronomy
+//
+//  Created by Wyatt Harrell on 5/18/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
