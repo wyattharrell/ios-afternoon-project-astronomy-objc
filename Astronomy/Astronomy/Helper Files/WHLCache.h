e144a2d4c89a506c1cec2048436941acae08e884
Wyatt Harrell <wyattaharrell@gmail.com>
2020-05-19 15:42:10 -0400

    created cache files

---
+++ Astronomy/Astronomy/Helper Files/WHLCache.h
@@ -0,0 +1,17 @@
+//
+//  WHLCache.h
+//  Astronomy
+//
+//  Created by Wyatt Harrell on 5/19/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WHLCache : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
